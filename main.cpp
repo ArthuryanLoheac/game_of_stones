76aaa1d08c52884afdbd7af6a31eba05c9ebb38d
Arthuryan Loheac <arthuryan.loheac-de-sainte-marie@epitech.eu>
2025-03-03 10:23:50 +0100

    🚀 Setup repo

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2025
+** Test
+** File description:
+** main
+*/
+
+#include <iostream>
+
+int main ( void )
+{
+    std::cout << "Hello World\n";
+    return 0;
+}
---
+++ myBuild.sh
@@ -0,0 +1,5 @@
+cmake . -B ./build -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release
+
+cmake --build build
+
+cp ./build/unit_tests ./
